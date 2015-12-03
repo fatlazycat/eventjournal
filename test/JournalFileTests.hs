4116eb6887e70e123e5ba59cce66c85b88a647e0
Graham Berks <graham@fatlazycat.com>
2015-12-03 17:08:20 +0000

    Test for osx page size

--- test/JournalFileTests.hs
+++ test/JournalFileTests.hs
@@ -2,12 +2,13 @@ module JournalFileTests where
 
 import           Test.Tasty
 import           Test.Tasty.Hspec
+import System.Posix.Memory
 
 specTests :: IO TestTree
 specTests = testGroup "journal file tests" <$> sequence [temp2]
 
 temp2 :: IO TestTree
-temp2 = testSpec "file creation" $
-  describe "Addition" $ do
-    it "2 + 2 is 4" $
-      (2 + 2) `shouldBe` 4
+temp2 = testSpec "OSX Specific - needs changing" $
+  describe "Page size" $ do
+    it "we expect page size to be 4096" $
+      sysconfPageSize `shouldBe` 4096
