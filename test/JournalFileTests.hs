ef5648ea854cc91dd6e4701d78ab0ac0f5523024
Graham Berks <graham@fatlazycat.com>
2015-12-04 07:29:34 +0000

    test for page size

--- test/JournalFileTests.hs
+++ test/JournalFileTests.hs
@@ -1,11 +1,19 @@
 module JournalFileTests where
 
+import           System.Posix.Memory
+import JournalFile
 import           Test.Tasty
 import           Test.Tasty.Hspec
-import System.Posix.Memory
 
 specTests :: IO TestTree
-specTests = testGroup "journal file tests" <$> sequence [temp2]
+specTests = testGroup "journal file tests" <$>
+            sequence [pageSize, temp2]
+
+pageSize :: IO TestTree
+pageSize = testSpec "Page sign alignment" $
+  describe "Page size" $ do
+    it "Ptr 0 and 100 bytes = 0 and page size" $
+      syncPoints 0 100 sysconfPageSize `shouldBe` (0,sysconfPageSize)
 
 temp2 :: IO TestTree
 temp2 = testSpec "OSX Specific - needs changing" $
